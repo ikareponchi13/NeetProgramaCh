4ddf2b5636bd67c96e9f31b9cdfadd73dd498924
gorira55 <43261187+gorira55@users.noreply.github.com>
2021-03-06 11:01:30 +0900

    検索機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :delete_all
+
+    def self.search(search)
+        return Post.all unless search
+        Post.where(['title LIKE ?', "%#{search}%"])
+    end
+
 end
